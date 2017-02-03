f04d077ea5fe018782e5441ccdf1874f0a386e55
zhaofeng-shu33 <616545598@qq.com>
2017-02-03 19:06:44 +0800

    js revised,tiny change,for testing github page only

--- js/ProcessingSearchJQueryVersion.js
+++ js/ProcessingSearchJQueryVersion.js
@@ -90,9 +90,9 @@ $(document).ready(function () {
 
     if (BrowersType == "Chrome") {
         //change the WordList_11.xml to WordList_11.html
-        if (!isIndexPage) {
-            self.parent.frames["wordList"].location = "Wordlist_11.html";
-        }
+      //  if (!isIndexPage) {
+      //      self.parent.frames["wordList"].location = "Wordlist_11.html";
+      //  }
         $.ajax({
             url: 'Wordlist_11.xml',
             type: 'GET',
