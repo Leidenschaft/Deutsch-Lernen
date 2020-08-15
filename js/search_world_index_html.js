c52156544c9a691d660530e81ff8cba8fe4af178
zhaofeng-shu33 <616545598@qq.com>
2020-08-15 14:32:10 +0800

    move some functions out

--- js/search_world_index_html.js
+++ js/search_world_index_html.js
@@ -1,106 +1,93 @@
 ﻿var jqueryFunction;
 var getWord_form;
-var search_result_list=[];
-$(document).ready(function () {
-    var search_mode = 'word_search';
-    $("#test_button").click(function () {
-        if (this.innerHTML == 'Test your vocabulary') {//open Test interface
-            this.innerHTML = '关闭测试界面'
-            self.parent.frames["editing_frame"].location = "client_form/test_interface_config.html";
-            self.parent.change_editing_frame("editing");
+var search_result_list = [];
+var search_mode = 'word_search';
 
+function ChangeContent() {
+    if (search_mode == 'word_search') {
+        var newFileName = GetAddress();
+            if (newFileName == null) {
+                newFileName = '1.xml';
+            }
+            self.location = 'main_view.html?wordAddress=' + newFileName;
+        
+    }
+    else {//example search, ajax request
+        $.ajax({
+            url: '../../corpus_service/example_search',
+            type: 'GET',
+            data:{
+                queried_word:$("#searchField").val()
+            },
+            dataType: 'json',
+            timeout: 1000,
+            cache: false,
+            error: function (xhr, status, errorThrown) {
+                alert("status: " + status + "\n errorThrown " + errorThrown);
+            },
+            success: function (json_data) {
+                var my_example_list = json_data.example_list;
+                if (my_example_list) {
+                    $("#resTree").html('');
+                    self.parent.document.getElementById('word_example_view').setAttribute('rows', "50%,50%");
+                    for (var i = 1; i <= my_example_list.length; i++) {
+                        var one_item_head = $("<span/>", { 'html': i.toString() + ': ', 'style': 'color:red' });
+                        var one_item = $("<p/>");
+                        one_item.append(one_item_head);
+                        one_item.append($("<span/>", { 'html': my_example_list[i - 1] }));
+                        $("#resTree").append(one_item);
+                    }
+                }
+                else {
+                    alert("queried word not found in corpus.");
+                }
+            }
+        });
+
+
+    }
+}
+jqueryFunction = function SearchContent(possibleWord) {
+    $("#searchField").val(possibleWord);
+    return GetAddress();
+}
+function GetAddress() {
+    var hasTheWord = 0;
+    var wordform_queried = $("#searchField").val();
+    for (var cnt = 0; cnt < dic.length;cnt++) {
+        if (dic[cnt].wordform == wordform_queried) {
+            hasTheWord = 1;
+            break;
         }
-        else {
-            self.parent.frames["editing_frame"].location = 'client_form/editing_interface.html';
-            self.parent.change_editing_frame("viewing");
-            this.innerHTML = 'Test your vocabulary';
+    }
+    if (hasTheWord == 1) {
+        LastSearchedWord = wordform_queried;
+        return dic[cnt].address;
+    }
+    else {
+        return null;
+    }
+}
+getWord_form = function get_word_form(wordAddr) {
+    var hasTheWord = 0;
+    for (var cnt = 0; cnt < dic.length; cnt++) {
+        if (dic[cnt].address == wordAddr) {
+            hasTheWord = 1;
+            break;
         }
-    });
- 
+    }
+    if (hasTheWord == 1) {
+        return dic[cnt].wordform;
+    }
+    else {
+        return null;
+    }
+
+}
+$(document).ready(function () {
     $("button").click(function () {
             ChangeContent();
     });
-    function ChangeContent() {
-        if (search_mode == 'word_search') {
-            var newFileName = GetAddress();
-                if (newFileName == null) {
-                    newFileName = '1.xml';
-                }
-                self.location = 'main_view.html?wordAddress=' + newFileName;
-           
-        }
-        else {//example search, ajax request
-            $.ajax({
-                url: '../../corpus_service/example_search',
-                type: 'GET',
-                data:{
-                    queried_word:$("#searchField").val()
-                },
-                dataType: 'json',
-                timeout: 1000,
-                cache: false,
-                error: function (xhr, status, errorThrown) {
-                    alert("status: " + status + "\n errorThrown " + errorThrown);
-                },
-                success: function (json_data) {
-                    var my_example_list = json_data.example_list;
-                    if (my_example_list) {
-                        $("#resTree").html('');
-                        self.parent.document.getElementById('word_example_view').setAttribute('rows', "50%,50%");
-                        for (var i = 1; i <= my_example_list.length; i++) {
-                            var one_item_head = $("<span/>", { 'html': i.toString() + ': ', 'style': 'color:red' });
-                            var one_item = $("<p/>");
-                            one_item.append(one_item_head);
-                            one_item.append($("<span/>", { 'html': my_example_list[i - 1] }));
-                            $("#resTree").append(one_item);
-                        }
-                    }
-                    else {
-                        alert("queried word not found in corpus.");
-                    }
-                }
-            });
-
-
-        }
-    }
-    jqueryFunction = function SearchContent(possibleWord) {
-        $("#searchField").val(possibleWord);
-        return GetAddress();
-    }
-    function GetAddress() {
-        var hasTheWord = 0;
-        var wordform_queried = $("#searchField").val();
-        for (var cnt = 0; cnt < dic.length;cnt++) {
-            if (dic[cnt].wordform == wordform_queried) {
-                hasTheWord = 1;
-                break;
-            }
-        }
-        if (hasTheWord == 1) {
-            LastSearchedWord = wordform_queried;
-            return dic[cnt].address;
-        }
-        else {
-            return null;
-        }
-    }
-    getWord_form = function get_word_form(wordAddr) {
-        var hasTheWord = 0;
-        for (var cnt = 0; cnt < dic.length; cnt++) {
-            if (dic[cnt].address == wordAddr) {
-                hasTheWord = 1;
-                break;
-            }
-        }
-        if (hasTheWord == 1) {
-            return dic[cnt].wordform;
-        }
-        else {
-            return null;
-        }
-
-    }
     $("#searchField").autocomplete({
         source: availableTags
     });
@@ -113,7 +100,6 @@ $(document).ready(function () {
         self.parent.document.getElementById('word_example_view').setAttribute('rows', "10%,90%");
         $("#searchField").autocomplete({ source: availableTags });
     });
-
     $("#searchField").keypress(function (e) {
             var keynum;
             keynum = e.keyCode;
